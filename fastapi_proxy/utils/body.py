7b2661c50d1f7dbbb72465938f9a807e47ef9bd0
UncleVasya <unclevasyaai@gmail.com>
2024-01-22 00:18:10 +0300

    exclude unset

--- fastapi_proxy/utils/body.py
+++ fastapi_proxy/utils/body.py
@@ -12,7 +12,7 @@ async def unzip_body_object(
         response_body_dict = {}
         for key in necessary_params:
             value = all_params.get(key)
-            _body_dict = await serialize_response(response_content=value)
+            _body_dict = await serialize_response(response_content=value, exclude_unset=True)
             response_body_dict.update(_body_dict)
         return JsonPayload(value=response_body_dict, dumps=ujson.dumps)
     return None
